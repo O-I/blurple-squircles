ada86edf5dfcffe616ec8938549f2fffab513092
Rahul Horé <raevyn_@hotmail.com>
2017-01-28 21:06:12 -0500

    Refactor handler to use status function

--- src/blurple_squircles/core.clj
+++ src/blurple_squircles/core.clj
@@ -1,9 +1,11 @@
 (ns blurple-squircles.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn status [req]
+  {:status 200
+   :body "Blurple Squircles"
+   :headers {}})
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req]
-                     {:status 200
-                      :body "Blurple Squircles"
-                      :headers {}})
+  (jetty/run-jetty status
                    {:port (Integer. port)}))
