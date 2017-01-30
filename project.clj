f9cc11c108e94aadee05287d121987f1c55d21ca
Rahul Horé <raevyn_@hotmail.com>
2017-01-30 07:28:15 -0500

    Add hiccup dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.5.1"]
-                 [compojure "1.5.2"]]
+                 [compojure "1.5.2"]
+                 [hiccup "1.0.5"]]
   :min-lein-version "2.0.0"
   :uberjar-name "blurple-squircles.jar"
   :main blurple-squircles.core
