f463bb05224ccc978b061f8acf050c28d6b0b701
Rahul Horé <raevyn_@hotmail.com>
2017-01-29 20:40:09 -0500

    Respond with 200 for root path only; 404 otherwise

--- src/blurple_squircles/core.clj
+++ src/blurple_squircles/core.clj
@@ -1,10 +1,14 @@
 (ns blurple-squircles.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn status [req]
-  {:status 200
-   :body "Blurple Squircles"
-   :headers {}})
+(defn status [{:keys [uri] :as req}]
+  (if (= uri "/")
+    {:status 200
+     :body "Blurple Squircles"
+     :headers {}}
+    {:status 404
+     :body "Shape and/or color not found"
+     :headers {}}))
 
 (defn -main [port]
   (jetty/run-jetty status
